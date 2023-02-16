4c1775d492fb4b98537e4142103abf57aa0247d1
stoneYuan3 <jackyuan110@gmail.com>
2023-02-16 12:45:50 -0800

    TEST, set up bottom navigation according to https://www.youtube.com/watch?v=OV25x3a55pk 112-02-16

--- app/src/main/java/com/example/gardenmananger_iat359finalproject/Activity_main_plantManangement.java
+++ app/src/main/java/com/example/gardenmananger_iat359finalproject/Activity_main_plantManangement.java
@@ -1,16 +1,49 @@
 package com.example.gardenmananger_iat359finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+//bottom navigation reference: https://www.youtube.com/watch?v=OV25x3a55pk
+
 public class Activity_main_plantManangement extends AppCompatActivity {
+
+    BottomNavigationView nav_bottom;
+
+    Frag_plantManange fragPlantManange=new Frag_plantManange();
+    Frag_records fragRecords=new Frag_records();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nav_bottom=findViewById(R.id.nav_bottom);
+
+        //set layout to plant manangement layout by default
+        getSupportFragmentManager().beginTransaction().replace(R.id.layout_main,fragPlantManange).commit();
+
+        //listen to changes on icons in nav_bottom and set layout accordingly
+        nav_bottom.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.button_nav_plants:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.layout_main,fragPlantManange).commit();
+                        return true;
+                    case R.id.button_nav_records:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.layout_main,fragRecords).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void gotoRecord(){
---
+++ app/src/main/java/com/example/gardenmananger_iat359finalproject/Frag_plantManange.java
@@ -0,0 +1,21 @@
+package com.example.gardenmananger_iat359finalproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Frag_plantManange extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_plant_manange, container, false);
+    }
+}
