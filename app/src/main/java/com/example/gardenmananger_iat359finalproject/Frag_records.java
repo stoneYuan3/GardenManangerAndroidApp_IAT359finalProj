5eede25906fd7dffc7d031c6f68c15f98383e149
Josh <joshchin8880@gmail.com>
2023-02-23 19:33:08 -0800

    Added activity for adding records

--- app/src/main/java/com/example/gardenmananger_iat359finalproject/Frag_records.java
+++ app/src/main/java/com/example/gardenmananger_iat359finalproject/Frag_records.java
@@ -1,5 +1,7 @@
 package com.example.gardenmananger_iat359finalproject;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +9,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-public class Frag_records extends Fragment {
-
+public class Frag_records extends Fragment implements View.OnClickListener {
+Button addRecord;
+Context context;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        addRecord = container.findViewById(R.id.addRecordButton);
+        context = container.getContext();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_frag_records, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
